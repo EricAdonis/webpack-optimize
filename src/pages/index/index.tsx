373870bce88bed473589cc0bfb0ace50bbafdb81
EricAdonis <ericadonis2712@gmail.com>
2020-01-09 23:59:56 +0700

    .

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,14 @@
 import React, { FC } from 'react'
 
-const Index: FC = () => <div>Index</div>
+interface Props {
+	name?: string
+	isAdmin?: boolean
+}
+
+const Index: FC<Props> = ({ name, isAdmin }) => (
+	<div>
+		Index {name}: {isAdmin}
+	</div>
+)
 
 export default Index
