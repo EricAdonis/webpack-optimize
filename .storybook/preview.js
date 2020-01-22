52a2de79edbbb15e45d971c4c84b6177568ff4eb
EricAdonis <ericadonis2712@gmail.com>
2020-01-23 00:28:47 +0700

    .

---
+++ .storybook/preview.js
@@ -0,0 +1,33 @@
+const { addParameters, addDecorator, configure } = require('@storybook/react')
+const {
+	DEFAULT_VIEWPORT,
+	INITIAL_VIEWPORTS,
+	MINIMAL_VIEWPORTS,
+} = require('@storybook/addon-viewport')
+const { withInfo } = require('@storybook/addon-info')
+const registerRequireContextHook = require('babel-plugin-require-context-hook/register')
+
+registerRequireContextHook()
+const req = global.__requireContext(
+	__dirname,
+	'../src/components',
+	true,
+	/\.stories\.tsx$/
+)
+function loadStories() {
+	req.keys().forEach(filename => req(filename))
+}
+
+configure(loadStories, module)
+
+addParameters({
+	viewport: {
+		viewports: {
+			DEFAULT_VIEWPORT,
+			...INITIAL_VIEWPORTS,
+			...MINIMAL_VIEWPORTS,
+		},
+	},
+})
+
+addDecorator(withInfo({ header: false, inline: true, source: true }))
